da07e97389251f280cb989f27029f2d889e40e99
Adn <nimouhq@gmail.com>
2023-08-21 17:54:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# mass-dm-friend-tools
+ tools in python for dm all your friend 
---
+++ main.py
@@ -0,0 +1,31 @@
+import discord
+import json
+
+mdmbot = discord.Client()
+
+with open("./config.json", "r") as config:
+    get = json.load(config)
+    token = get["token"]
+    message = get["message"]
+
+@mdmbot.event
+async def on_connect():
+  print("User Logged Into:")
+  print("-----------------")
+  print(mdmbot.user.name)
+  print(mdmbot.user.id)
+  print("-----------------")
+  print("Started massing..")
+  print("-----------------")
+
+  for user in mdmbot.user.friends:
+    try:
+
+      await user.send(message)
+
+      print(f"Successfully message sent to: {user.name}")
+    except:
+       print(f"Failed to send message to: {user.name}")
+  print(f"{mdmbot.user.name} hass finished mdming!")
+
+mdmbot.run(token, bot=False)
